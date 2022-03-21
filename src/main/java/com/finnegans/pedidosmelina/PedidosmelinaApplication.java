1f8d942cd452e1af9b5d3044e168dcf416725f8b
melinarias <mebuddy88@gmail.com>
2022-03-21 01:08:33 -0300

    Inicio de proyecto

---
+++ src/main/java/com/finnegans/pedidosmelina/PedidosmelinaApplication.java
@@ -0,0 +1,13 @@
+package com.finnegans.pedidosmelina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosmelinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosmelinaApplication.class, args);
+	}
+
+}
